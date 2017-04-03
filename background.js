51cb2388e916f317d417968b26d64d5617402059
tvmdragon <tavanminh97@gmail.com>
2017-04-03 09:50:52 +0700

    remove ads

--- background.js
+++ background.js
@@ -3,6 +3,8 @@
 // found in the LICENSE file.
 // Global variables only exist for the life of the page, so they get reset
 // each time the page is unloaded.
+
+
 var counter = 1;
 
 var lastTabId = -1;
@@ -49,14 +51,14 @@ chrome.commands.onCommand.addListener(function (command) {
 
     chrome.tabs.query({}, function (tabs) {
         for (var i = 0; i < tabs.length; i++) {
-            if (tabs[i].url.indexOf("youtube.com") > -1 
-                ||tabs[i].url.indexOf("soundcloud.com") > -1 ) {
+            if (tabs[i].url.indexOf("youtube.com") > -1
+                || tabs[i].url.indexOf("soundcloud.com") > -1) {
                 if (command == "next" || command == "global_next" || command == "global_next2") {
                     chrome.tabs.executeScript(tabs[i].id, {
                         code: "document.querySelector('.ytp-next-button, .playControls__next').click();"
                     });
                 }
-                if (command == "pause" || command == "pause-global" ) {
+                if (command == "pause" || command == "pause-global") {
                     chrome.tabs.executeScript(tabs[i].id, {
                         code: "document.querySelector('.ytp-play-button, .playControls__play').click();"
                     });
