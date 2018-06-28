1bd96097eb553805feba23fea2b0b5e1e0ad700f
tvmdragon <tavanminh97@gmail.com>
2018-06-28 14:25:15 +0700

    fix loi

--- background.js
+++ background.js
@@ -32,19 +32,19 @@ chrome.runtime.onInstalled.addListener(function () {
     // need to persist across page reloads.
     localStorage.counter = 1;
 
-    // Register a webRequest rule to redirect bing to google.
-    var wr = chrome.declarativeWebRequest;
-    chrome.declarativeWebRequest.onRequest.addRules([{
-        id: "0",
-        conditions: [new wr.RequestMatcher({
-            url: {
-                hostSuffix: "bing.com"
-            }
-        })],
-        actions: [new wr.RedirectRequest({
-            redirectUrl: "http://google.com"
-        })]
-    }]);
+    // // Register a webRequest rule to redirect bing to google.
+    // var wr = chrome.declarativeWebRequest;
+    // chrome.declarativeWebRequest.onRequest.addRules([{
+    //     id: "0",
+    //     conditions: [new wr.RequestMatcher({
+    //         url: {
+    //             hostSuffix: "bing.com"
+    //         }
+    //     })],
+    //     actions: [new wr.RedirectRequest({
+    //         redirectUrl: "http://google.com"
+    //     })]
+    // }]);
 });
 
 chrome.commands.onCommand.addListener(function (command) {
